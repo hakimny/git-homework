79a6a79f43ac13fd6174e9d694280ae6bc97291c
Hakim TAMGALT <htamgalt@groupeproaction.com>
2019-10-16 21:54:38 -0400

    Add print all data related to all patients, 1 at a time

--- homework.py
+++ homework.py
@@ -6,4 +6,12 @@ maxData = np.max(data)
 minData = np.min(data)
 meanData = np.mean(data)
 stdData = np.std(data)
-print(f'MaxValue = {maxData}! | MinValue = {minData} | Mean Value = {meanData} | Standard Deviation = {stdData} ')
+
+print(f'Max Value = {maxData:.2f}! | Min Value = {minData:.2f} | Mean Value = {meanData:.2f} | Standard Deviation = {stdData:.2f} ')
+
+# Max,Min, Mean, Standard Deviation per patient
+
+i = 1
+for patient in data:
+	print(f'Patient {i}:  Max Value = {np.max(patient):.2f} | Min Value =  {np.min(patient):.2f} | Mean Value = {np.mean(patient):.2f} | Standard Deviation = {np.std(patient):.2f}')
+	i = i + 1
