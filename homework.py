fcffc172bd5b7c28574896a58b97d7abb2a9116e
Hakim TAMGALT <htamgalt@groupeproaction.com>
2019-10-16 21:37:54 -0400

    Add math functions to calculate max,min, mean, standard deviation

--- homework.py
+++ homework.py
@@ -1,4 +1,9 @@
-import numpy
+import numpy as np
 
-data = numpy.loadtxt(fname='data.csv', delimiter=',')
+data = np.loadtxt(fname='data.csv', delimiter=',')
 print(data)
+maxData = np.max(data)
+minData = np.min(data)
+meanData = np.mean(data)
+stdData = np.std(data)
+print(f'MaxValue = {maxData}! | MinValue = {minData} | Mean Value = {meanData} | Standard Deviation = {stdData} ')
