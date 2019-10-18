00d8922c8f7f620ebbe54e504839e5c29e552e72
Hakim TAMGALT <htamgalt@groupeproaction.com>
2019-10-17 21:19:09 -0400

    test visualizing average inflammation per day

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as matplot
 
 data = np.loadtxt(fname='inflammation-01.csv', delimiter=',')
 print(data)
@@ -16,5 +16,19 @@ i = 1
 for patient in data:
 	print(f'Patient {i}:  Max Value = {np.max(patient):.2f} | Min Value =  {np.min(patient):.2f} | Mean Value = {np.mean(patient):.2f} | Standard Deviation = {np.std(patient):.2f}')
 	i = i + 1
-print(f'Max Data for each patient: {np.max(data, axis=1)}')
-print(f'Mean Value for each day: {np.mean(data, axis=0)}')
+
+max_inflammation_per_patient = np.max(data,axis=1)
+avg_inflammation_per_day = np.mean(data, axis=0)
+min_inflammation_per_patient = np.min(data,axis=1)
+# print(f'Max Data for each patient: {max_inflammation_per_patient}')
+# print(f'Mean Value for each day: {avg_inflammation_per_day}')
+
+# Visualize Data
+
+
+# image = matplotlib.pyplot.imshow(data)
+# matplotlib.pyplot.show()
+
+
+avg_plot = matplot.plot(avg_inflammation_per_day)
+matplot.show()
