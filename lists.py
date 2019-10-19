3572c0317b7ab440bdbd7ef7b1598c9bdffc3bde
Hakim TAMGALT <htamgalt@groupeproaction.com>
2019-10-18 21:06:30 -0400

    Add Slicing section

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+#Nested Lists
+
 x = [['pepper', 'zucchini', 'onion'],
      ['cabbage', 'lettuce', 'garlic'],
      ['apple', 'pear', 'banana']]
@@ -13,3 +15,26 @@ print(f'x: {x}')
 new_list = [0,1,2,3]
 new_list.reverse()
 print(f'reversed_list: {new_list}')
+
+# Create list from String
+
+str = "hello"
+list = []
+for char in str:
+	list.append(char)
+print(list)
+
+# Slicing
+
+string_for_slicing = "Observation date: 02-Feb-2013"
+list_for_slicing = [["fluorine", "F"],
+                    ["chlorine", "Cl"],
+                    ["bromine", "Br"],
+                    ["iodine", "I"],
+                    ["astatine", "At"]]
+start_slicing_index = len(string_for_slicing) - 4
+print(string_for_slicing[start_slicing_index:])
+
+for item in list_for_slicing:
+	start_slicing_index = len(item) -4
+	print(item[start_slicing_index:])
